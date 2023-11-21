51e8701faae33660555273b2461b30ac198a1db4
Lian <lcb_faust@naver.com>
2023-11-21 09:45:55 +0900

    test

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ import ApiTest from "./ch13_API_PublicDataTest/ApiTest";
 import ApiTestKoreaNews from "./ch13_API_PublicDataTest/ApiTestKoreaNews";
 import NewsPage from "./ch13_API_PublicDataTest/page/NewsPage";
 import TestColorMain from "./ch14_ContextAPITest/TestColorMain";
-import FireMain from "./ch15_FirebaseTest/FireMain";
+// import FireMain from "./ch15_FirebaseTest/FireMain";
 
 function App() {
   return (
@@ -68,7 +68,7 @@ function App() {
         <Route path="newsPageTest/:category" element={<NewsPage />} />
         <Route path="contextAPITest" element={<TestColorMain />} />
         {/* <Route path="contextAPITest" element={<TestColorBox />} /> */}
-        <Route path="firebaseTest" element={<FireMain />} />
+        {/* <Route path="firebaseTest" element={<FireMain />} /> */}
       </Routes>
     </BrowserRouter>
   );
